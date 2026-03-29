d34b508bab0711dbed8ed7214745e861fc4e387a
rigan240188 <cisneros.5@hotmail.com>
2026-03-29 01:34:29 -0600

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,39 @@
+import { db } from "./firebase.js"
+import { collection, getDocs } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+
+const boton = document.getElementById("abrirChat")
+const chat = document.getElementById("chat")
+const enviar = document.getElementById("enviar")
+
+boton.onclick = () =>{
+chat.style.display = chat.style.display === "none" ? "flex":"none"
+}
+
+enviar.onclick = async () =>{
+
+let entrada = document.getElementById("entrada")
+let mensaje = entrada.value.toLowerCase()
+
+document.getElementById("mensajes").innerHTML +=
+"<p><b>Tú:</b> "+mensaje+"</p>"
+
+let respuesta = "No entiendo tu pregunta"
+
+const querySnapshot = await getDocs(collection(db,"chatbot"))
+
+querySnapshot.forEach((doc)=>{
+
+let data = doc.data()
+
+if(mensaje.includes(data.palabra)){
+respuesta = data.respuesta
+}
+
+})
+
+document.getElementById("mensajes").innerHTML +=
+"<p><b>Bot:</b> "+respuesta+"</p>"
+
+entrada.value=""
+}
---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiMjMGA3_cWFILNtoVK96xxDNXzQe9gOg",
+    authDomain: "pronub.firebaseapp.com",
+    projectId: "pronub",
+    storageBucket: "pronub.firebasestorage.app",
+    messagingSenderId: "502044938131",
+    appId: "1:502044938131:web:e6bb205f3d41a2438d2266",
+    measurementId: "G-8RV42Y19PE"
+  };
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { db }
